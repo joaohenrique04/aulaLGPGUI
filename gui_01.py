a3304f8973c1e59f2331fdae0fc72b312c70fd8c
JOÃO HENRIQUE BARBOSA FERNANDES ALENCAR <2023002959@IFAL-MACEIO.INTRANET>
2023-12-06 20:15:40 -0300

    commit 1

---
+++ gui_01.py
@@ -0,0 +1,25 @@
+## TESTE DE GUI COM JANELA E BOTAO
+
+from tkinter import *
+
+def clicked():
+    lbl.configure(text="você clicou ;)")
+
+window = Tk()
+window.title("Bem-vindo, NERD B)")
+window.geometry("350x200")
+
+# lbl = Label(window, text="Olá")
+# lbl.pack()
+
+lbl=Label(window, text="Isso é um Label Widget", fg="red",
+          font=("Helvetica", 16))
+lbl.place(x=60, y=50)
+
+txtfld=Entry(window, text="Isso é uma caixa de entrada po", bd=2)
+txtfld.place(x=80, y=150)
+
+btn = Button(window, text="me clica por favor", command=clicked)
+btn.pack()
+
+window.mainloop()
